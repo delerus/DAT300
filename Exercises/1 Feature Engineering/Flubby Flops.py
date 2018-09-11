4fdffaa7791767516b68996b1d199844064ecedd
Lars Erik Ødegaard <larserikodegaard@Larss-MacBook-Pro.local>
2018-09-11 17:23:43 +0200

    Done the first exercise

---
+++ Exercises/1 Feature Engineering/Flubby Flops.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  7 15:08:29 2018
+
+@author: kristl
+"""
+
+"""
+ As the world leading producer of Flubby Flops (TM) you want to predict
+ the flubberiness from the raw material analyses and input settings of 
+ the Flubmaster EX. The following features are available:
+ + Raw materials
+     - flostard
+     - flüber
+     - lard
+ + Process attributes
+     - floppiness
+     - process start
+     - boiling stop
+     - stretching stop
+ + Response
+     - flubberiness
+ 
+ You have been introduced to several techniques in feature engineering in 
+ your days at the university with regard to deriving new features, 
+ recoding, transforming, and making interactions. ++
+ 
+ Use the full dataset to predict the response (ordinary linear regression
+ should suffice, but other tools may work). Apply your feature engineering
+ tools and try to achieve predictions that are less than 10**-8 off target.
+ During the exploration, visualise and ponder on the attributes of the
+ available features.
+ 
+ Data are available as a CSV file.
+ """
---
+++ Exercises/1 Feature Engineering/OpenML_Titanic.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 10 11:32:11 2018
+
+@author: kristl
+"""
+
+"""
+OpenML - Titanic survival (simplified)
+
+Use the PMLB version of the Titanic data: https://www.openml.org/d/40704.
+- Download through OpenML API
+- Predict survival in a 10-fold cross-validation
+- Check if tricks from feature engineering improve predictions
+- Compare to uploaded "Supervised Classification on Titanic" tasks
+
+Example code for using OpenML in Python:
+https://github.com/openml/openml-python/blob/master/examples/OpenML_Tutorial.ipynb
+"""
